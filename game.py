332339e6dfc5cd49b8415e77b34a4aac65f1c974
Champson8 <eha123lha456@gmail.com>
2026-05-12 09:46:43 -0700

    make mutationsCount dependent on board size

--- game.py
+++ game.py
@@ -270,39 +270,40 @@ class Round:
 
     def _setBoardConfig(self):
         difficulty = (self.roundNumber - 1) / (self.settings.totalRounds - 1)
+        mutationsMultiplier = (self.settings.boardSize - 4) // 2 + 1
 
         if difficulty <= 0.2:
             self.boardConfig = {
                 "initialState": InitialBoardState.BLANK,
-                "mutationsCount": randint(2, 5),
+                "mutationsCount": randint(2, 5) * mutationsMultiplier,
                 "spreadFactor": 0.35,
                 "symmetryWeight": 1,
             }
         elif difficulty <= 0.4:
             self.boardConfig = {
                 "initialState": InitialBoardState.PATTERN,
-                "mutationsCount": randint(3, 7),
+                "mutationsCount": randint(3, 7) * mutationsMultiplier,
                 "spreadFactor": 0.2,
                 "symmetryWeight": 0.85,
             }
         elif difficulty <= 0.7:
             self.boardConfig = {
                 "initialState": InitialBoardState.PATTERN,
-                "mutationsCount": randint(5, 9),
+                "mutationsCount": randint(5, 9) * mutationsMultiplier,
                 "spreadFactor": 0.65,
                 "symmetryWeight": 0.4,
             }
         elif difficulty <= 0.9:
             self.boardConfig = {
                 "initialState": InitialBoardState.PATTERN,
-                "mutationsCount": randint(8, 12),
+                "mutationsCount": randint(8, 12) * mutationsMultiplier,
                 "spreadFactor": 0.8,
                 "symmetryWeight": 0.1,
             }
         else:
             self.boardConfig = {
                 "initialState": InitialBoardState.RANDOM,
-                "mutationsCount": randint(10, 15),
+                "mutationsCount": randint(10, 15) * mutationsMultiplier,
                 "spreadFactor": 0.9,
                 "symmetryWeight": 0,
             }
